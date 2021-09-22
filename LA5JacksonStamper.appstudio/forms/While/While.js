a3cec3fdd9e976733d749d78d0dde96222dd695b
jackson-stamper <jacksonstamper32@gmail.com>
2021-09-22 15:03:22 -0500

    This is my work done for While

---
+++ LA5JacksonStamper.appstudio/forms/While/While.js
@@ -0,0 +1,19 @@
+/* 
+First program - create a program that uses a 'while' loop and outputs each iteration as shown: 
+form named 'while'
+output to console using a template literal: 
+This is loop iteration 1.
+This is loop iteration 2.
+etc....
+*/
+
+/*
+let i = 1
+while (i < 10) {
+  console.log(`This is iteration ${i}.`)
+  i++
+}
+*/
+
+
+
