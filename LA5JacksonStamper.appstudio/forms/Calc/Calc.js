b32eef2739ca89d64ed61447f3eec178c3c1b0e0
jackson-stamper <jacksonstamper32@gmail.com>
2021-09-22 15:06:14 -0500

    Commits for Calc branch

---
+++ LA5JacksonStamper.appstudio/forms/Calc/Calc.js
@@ -0,0 +1,22 @@
+/*
+Third program - create a program that uses a function to add two numbers from the user and return their sum to the main (calling) program.  
+form named ‘calc’
+function named ‘Calculate’ that takes two parameters named 'num1' and 'num2' and returns their sum to the main program.
+Do NOT use any global variables in the function (only use variables declared in the function and the functin parameters). 
+in the main program, gets two numbers from user and send them to the function.
+Note: remember to catch the answer that the function returns back to the main program.
+in the main program, output answer to console using a template literal in this format:
+        The sum of X and Y is Z.
+*/
+/*
+function Calculate(num1, num2){
+  let sum = num1 + num2
+  return sum
+}
+
+n1 = Number(prompt("Enter number 1 value"))
+n2 = Number(prompt("Enter number 2 value"))
+
+let answer = Calculate(n1, n2)
+console.log(`The sum of ${n1} and ${n2} is ${answer}`)
+*/
