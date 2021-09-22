547d2b88ec76fc926bfca4e13127541af24fa28f
jackson-stamper <jacksonstamper32@gmail.com>
2021-09-22 14:13:58 -0500

    first commit

---
+++ LA5JacksonStamper.appstudio/forms/Form1/Form1.js
@@ -0,0 +1,8 @@
+/* 
+First program - create a program that uses a 'while' loop and outputs each iteration as shown: 
+form named 'while'
+output to console using a template literal: 
+This is loop iteration 1.
+This is loop iteration 2.
+etc....
+*/
